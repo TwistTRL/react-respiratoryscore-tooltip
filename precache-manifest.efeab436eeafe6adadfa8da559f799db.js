48f27b027e67cf93a790fd77126768fe9a544b95
Lingyu Zhou <zly@TRLDSC2.CHBOSTON.ORG>
2019-05-17 17:34:41 -0400

    Updates

---
+++ precache-manifest.efeab436eeafe6adadfa8da559f799db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7b851a00550f0e5a2f0",
+    "url": "/react-respiratoryscore-tooltip/static/js/runtime~main.100f255d.js"
+  },
+  {
+    "revision": "0f78f17ce1933dfdbe0b",
+    "url": "/react-respiratoryscore-tooltip/static/js/main.14701c8e.chunk.js"
+  },
+  {
+    "revision": "b2aeb0622029314b4a35",
+    "url": "/react-respiratoryscore-tooltip/static/js/2.b65446f0.chunk.js"
+  },
+  {
+    "revision": "0f78f17ce1933dfdbe0b",
+    "url": "/react-respiratoryscore-tooltip/static/css/main.e765f360.chunk.css"
+  },
+  {
+    "revision": "476435ca6f13da34c7b6905190440eef",
+    "url": "/react-respiratoryscore-tooltip/index.html"
+  }
+];
