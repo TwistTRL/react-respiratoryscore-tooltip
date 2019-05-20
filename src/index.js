74ec412b8d3482294bad53060746ebe53be20c17
Lingyu Zhou <zly@TRLDSC2.CHBOSTON.ORG>
2019-05-20 10:10:40 -0400

    added control for gh-pages demo

--- src/index.js
+++ src/index.js
@@ -3,13 +3,34 @@ import ReactDOM from 'react-dom';
 import RespiratoryScoresTooltip from "./lib/RespiratoryScoresTooltip";
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={pageX:100,pageY:100};
+  }
   render() {
-    return (<RespiratoryScoresTooltip location={"ICU"} timeStamp={new Date()}
-                                      ECMOVariables={{ECMO_Flow_Rate_Weight_Normalized:90}}
-                                      VADVariables={{Machine_Type: "RotaWare"}}
-                                      respiratoryVariables={{"RST":"PSV","PIP":28,"FIO2":30}}
-                                      pageX={100} pageY={100}
-              />)
+    let {pageX,pageY} = this.state;
+    return (
+      <>
+        pageX:
+        <input  type="range" min={0} max={1000} value={pageX}
+                onChange={(ev)=> {
+                            this.setState({pageX:Number.parseInt(ev.target.value)})
+                          }}
+        />
+        pageY:
+        <input  type="range" min={0} max={1000} value={pageY}
+                onChange={(ev)=> {
+                            this.setState({pageY:Number.parseInt(ev.target.value)})
+                          }}
+        />
+        <RespiratoryScoresTooltip location={"ICU"} timeStamp={new Date()}
+                                  ECMOVariables={{ECMO_Flow_Rate_Weight_Normalized:90}}
+                                  VADVariables={{Machine_Type: "RotaWare"}}
+                                  respiratoryVariables={{"RST":"PSV","PIP":28,"FIO2":30}}
+                                  pageX={pageX} pageY={pageY}
+        />
+      </>
+    );
   }
 }
 
